0af2da20c83b5aeb36c05373d62c6c918ac6e43a
Antônio Roberto  Silva <forevertonny@gmail.com>
2010-07-27 23:13:37 -0300

    outros...

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,7 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 Factory.define :user do |f|
+  f.sequence(:email){|n| "email#{n}@example.com" }
+  f.password "1234567"
+  f.password_confirmation "1234567"  
 end
